ae323ccaf01fdd1eafc3535d256c5c12f466a9b0
jccsISC <silva.jccs18@gmail.com>
2021-10-24 21:19:33 -0500

    Introduccion a la varianza

--- src/main/kotlin/kotlin_avanzado/genericos/MainGenerics.kt
+++ src/main/kotlin/kotlin_avanzado/genericos/MainGenerics.kt
@@ -21,23 +21,43 @@ fun main() {
     /**
      * Pensemos que tenemos un inventario así que tenemos lo siguiente
      * */
-    val mesaTipo1 = Mesa("Futura", 5, Dimension(1.8, 2.1, 1.5))
+    val mesaTipo1 = Mesa("Futura", 5, Dimension(2.8, 2.5, 2.5))
     val mesaTipo2 = Mesa("Brisa", 3, Dimension(2.5, 2.5, 1.7))
-    val sofaTipo1 = Sofa("Praga", 3, Dimension(5.2, 1.8, 1.6))
-    val sofaTipo2 = Sofa("Lisboa", 4, Dimension(6.1, 1.6, 1.7))
+    val sofaTipo1 = Sofa("Praga", 3, Dimension(6.2, 2.8, 2.6))
+    val sofaTipo2 = Sofa("Lisboa", 4, Dimension(6.1, 2.6, 2.7))
     val inventario = listOf(mesaTipo1, mesaTipo1, mesaTipo1, sofaTipo1, sofaTipo2, mesaTipo1, sofaTipo2, sofaTipo2, mesaTipo2)
 
     val numeroMesasTipo1 = numeroDeApariciones(mesaTipo1, inventario)
 
-    print("Mesas de tipo1 en existencia: $numeroMesasTipo1")
+    println("Mesas de tipo1 en existencia: $numeroMesasTipo1")
 
     /**
      * Si no declaramos restricciones en nuestras clases, funciones genericos puede hacer un mal uso de las mismas
      * como en este ejemplo, están guardando un string en lugar de un mueble
      * */
 //    val miCaja = Caja("sdasdsa", Dimension(1.8, 2.0, 3.0))
-    val miCaja = Caja(sofa, Dimension(1.8, 2.0, 3.0))
+    val miCaja = Caja(sofa, Dimension(6.8, 5.0, 3.0))
 
 
+    /**
+     * Relaciones de herencia que se crea entre objetos genéricos-------------------------------------------------------
+     * */
+    val miAny: Any = "String en Any"
+    val miString: String = "String"
+    printAny(miAny)
+    printAny(miString)
+    val miAnyList = mutableListOf(miAny)
+    val miStringList = mutableListOf(miString)
+    printListAny(miAnyList)
+//    printListAny(miStringList)
+}
 
+fun printAny(miObjeto: Any) {
+    println(miObjeto)
+}
+
+fun printListAny(miList: MutableList<Any>) {
+    for (x in miList) {
+        println(x)
+    }
 }
