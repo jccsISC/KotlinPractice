60b7468e28a1366bcbc020d0c22ad20a1eddfe1f
jccsISC <silva.jccs18@gmail.com>
2022-01-20 01:31:02 -0600

    FlatMapMerge

--- src/main/kotlin/kotlin_avanzado/coroutines/MainFlow.kt
+++ src/main/kotlin/kotlin_avanzado/coroutines/MainFlow.kt
@@ -154,10 +154,17 @@ fun main(args: Array<String>) {
         val ejemplo = (1..3).asFlow().map { requestFlow(it) }
     }*/
 
-    runBlocking {
+    /*runBlocking {
         val startTime = System.currentTimeMillis()
         (1..3).asFlow().onEach { delay(100) }
             .flatMapConcat { requestFlow(it) }
+            .collect {value-> println("$value at ${System.currentTimeMillis() - startTime} ms from start") }
+    }*/
+
+    runBlocking {
+        val startTime = System.currentTimeMillis()
+        (1..3).asFlow().onEach { delay(100) }
+            .flatMapMerge { requestFlow(it) }
             .collect {value-> println("$value at ${System.currentTimeMillis() - startTime} ms from start") }
     }
 }
